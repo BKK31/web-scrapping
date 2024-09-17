35ffe1a351c9c7d73524418431d4dd0234a0af9a
Bhargava K K <bhargavakk13@gmail.com>
2024-09-17 19:57:37 +0530

    Added chrome-driver compatibility for windows platform

--- webScrapper.py
+++ webScrapper.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service  # Import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import os
 
 driver_option = webdriver.ChromeOptions()
 driver_option.add_argument("-incognito")
-chromedriver_path = 'chrome-linux64.zip'
+
+# Uncomment the line corresponding to your operating system
+# chromedriver_path = './chromedriver'  # For Linux systems
+chromedriver_path = './chromedriver.exe'  # For Windows Systems
 
 def create_webdriver():
-    return webdriver.Chrome(executable_path=chromedriver_path, chrome_options=driver_option)
+    service = Service(executable_path=chromedriver_path)
+    return webdriver.Chrome(service=service, options=driver_option)
 
 # Open the website
 browser = create_webdriver()
